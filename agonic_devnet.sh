6a313c2002a21ded51f2ee6e2dc1d7aff98bbf92
Pavel <pavel.butenko@yahoo.com>
2023-03-03 16:06:12 +0200

    Update agonic_devnet.sh

--- agonic_devnet.sh
+++ agonic_devnet.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# wget -q -O agoric_mainnet.sh https://api.nodes.guru/agoric_mainnet.sh && chmod +x agoric_mainnet.sh && sudo /bin/bash agoric_mainnet.sh
+# wget -q -O agonic_devnet.sh https://github.com/paveldp/scripts/blob/main/agonic_devnet.sh && chmod +x agonic_devnet.sh && sudo /bin/bash agonic_devnet.sh
 . ~/.bashrc
 if [ ! $AGORIC_NODENAME ]; then
 	read -p "Enter node name: " AGORIC_NODENAME
@@ -18,67 +18,63 @@ chmod +x nodesgurulogo
 ./nodesgurulogo
 sleep 3
 
-curl https://deb.nodesource.com/setup_14.x | sudo bash
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt upgrade -y < "/dev/null"
-sudo apt install nodejs=14.* yarn build-essential jq git -y < "/dev/null"
+curl https://deb.nodesource.com/setup_16.x | sudo bash
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt upgrade -y
+sudo apt install nodejs=16.* yarn build-essential git jq -y
+node --version | grep 16
+yarn --version
 sleep 1
 
-wget -O go1.17.1.linux-amd64.tar.gz https://golang.org/dl/go1.17.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz && rm go1.17.1.linux-amd64.tar.gz
-cat <<'EOF' >> $HOME/.bash_profile
+sudo rm -rf /usr/local/go
+curl -L -o /tmp/go1.18.7.linux-amd64.tar.gz https://go.dev/dl/go1.18.7.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf /tmp/go1.18.7.linux-amd64.tar.gz
+cat <<'EOF' >>$HOME/.profile
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export GO111MODULE=on
 export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 EOF
-. $HOME/.bash_profile
-cp /usr/local/go/bin/go /usr/bin
-go version
-
-export GIT_BRANCH=agoric-3
-git clone https://github.com/Agoric/ag0
-cd ag0
-git checkout agoric-3.1
-make build
-. $HOME/.bash_profile
-cp $HOME/ag0/build/ag0 /usr/local/bin
-
-curl https://main.agoric.net/network-config > chain.json
+source $HOME/.profile
+go version | grep 1.18
+git clone https://github.com/Agoric/agoric-sdk
+cd agoric-sdk
+git checkout 162fdf2a51c54e2da89c8ed35f06cab09fb013d2
+yarn install && yarn build
+(cd packages/cosmic-swingset && make)
+agd version --long
+cd
+curl https://devnet.agoric.net/network-config > chain.json
 chainName=`jq -r .chainName < chain.json`
 echo $chainName
-
-ag0 init --chain-id $chainName $AGORIC_NODENAME
-curl https://main.agoric.net/genesis.json > $HOME/.agoric/config/genesis.json 
-ag0 unsafe-reset-all
+agd init --chain-id $chainName $AGORIC_NODENAME
+curl https://devnet.rpc.agoric.net/genesis | jq .result.genesis > $HOME/.agoric/config/genesis.json 
 peers=$(jq '.peers | join(",")' < chain.json)
 seeds=$(jq '.seeds | join(",")' < chain.json)
 echo $peers
 echo $seeds
 sed -i.bak 's/^log_level/# log_level/' $HOME/.agoric/config/config.toml
 sed -i.bak -e "s/^seeds *=.*/seeds = $seeds/; s/^persistent_peers *=.*/persistent_peers = $peers/" $HOME/.agoric/config/config.toml
-sudo tee <<EOF >/dev/null /etc/systemd/system/agoricd.service
+sudo tee <<EOF >/dev/null /etc/systemd/system/agd.service
 [Unit]
 Description=Agoric Cosmos daemon
 After=network-online.target
-
 [Service]
+# OPTIONAL: turn on JS debugging information.
+#SLOGFILE=.agoric/data/chain.slog
 User=$USER
-ExecStart=$(which ag0) start --log_level=info
+# OPTIONAL: turn on Cosmos nondeterminism debugging information
+#ExecStart=$HOME/go/bin/agd start --log_level=info --trace-store=.agoric/data/kvstore.trace
+ExecStart=$HOME/go/bin/agd start --log_level=warn
 Restart=on-failure
 RestartSec=3
-LimitNOFILE=65535
-
+LimitNOFILE=4096
 [Install]
 WantedBy=multi-user.target
 EOF
-. ~/.bash_profile
-sed -i '/\[telemetry\]/{:a;n;/enabled/s/false/true/;Ta};/\[api\]/{:a;n;/enable/s/false/true/;Ta;}' $HOME/.agoric/config/app.toml
-sed -i "s/prometheus-retention-time = 0/prometheus-retention-time = 60/g" $HOME/.agoric/config/app.toml
-sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025ubld\"/;" $HOME/.agoric/config/app.toml
-sed -i "s/prometheus = false/prometheus = true/g" $HOME/.agoric/config/config.toml
-sudo systemctl enable agoricd
+sudo systemctl enable agd
 sudo systemctl daemon-reload
-sudo systemctl restart agoricd
-echo 'Node status:'$(sudo service agoricd status | grep active)
+sudo systemctl start agd
+sudo journalctl -u agd -f
